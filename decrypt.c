93e7afe12bfab2ec8ab09151259812e89e868376
Ian Kinnes <kinnes16@up.edu>
2014-02-26 19:23:20 -0800

    bug fix in encrypt / decrypt

--- decrypt.c
+++ decrypt.c
@@ -4,16 +4,17 @@ void decrypt(char ** a)
 {
   int i=0;
   int j=0;
-  for(i; a[i]!=NULL; i++) { // lines
-    for(j; a[i][j] !='\0'; j++){ // chars in a line
-      if(a[i][j]>=33&&a[i][j]<=126){
-	if(a[i][j]==33) {
-	a[i][j] = 126;
-	} else {
-        a[i][j]--;
+  for(i; a[i]!=NULL;i++){
+    for(j=0;a[i][j]!='\0';j++) {
+      if (a[i][j] == 33)
+	{
+	  a[i][j] = 126;
+	}
+      else
+	{
+	  a[i][j]--;
 	}
       }
-    }
   }
   
   return;
--- encript.c
+++ encript.c
@@ -4,17 +4,18 @@ void encript(char ** a)
 {
   int i=0;
   int j=0;
-  for(i; a[i]!=NULL;i++){
-    for(j;a[i][j]!='\0';j++) {
-	  if(a[i][j]>=33&&a[i][j]<=126){
-	    if(a[i][j]==126){
-		  a[i][j]=33;
-		} else {
-	      a[i][j]++;
-	    }
-	  }
+  for(i = 0; a[i] != NULL;i++){
+    for(j = 0;a[i][j] != '\0'; j++) {
+      if (a[i][j] == 126)
+	{
+	  a[i][j] = 33;
+	}
+      else
+	{
+	  a[i][j]++;
 	}
       }
+  }
   return;
 }
 
